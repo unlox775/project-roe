82a3af44324f2ee6fc6ce7546ab1a8f9b3893ab7
Dave Buchanan <dave@joesvolcano.net>
2023-08-07 18:07:18 -0700

    Added bling and watching ChatGPT events

--- project-roe-bookmarklet.js
+++ project-roe-bookmarklet.js
@@ -1,6 +1,34 @@
 (function() {
     // Open a generic prompt to let the user choose either "whip" or "bard"
     let promptResult = prompt('Choose a character', 'whip');
+    promptResult = promptResult.toLowerCase();
+    promptResultTitle = promptResult.charAt(0).toUpperCase() + promptResult.slice(1);
+
+    //  If previous loaded, remove it
+    const idsToRemove = ['roe-send-button', 'roe-label'];
+    idsToRemove.forEach(id => {
+        if (document.getElementById(id)) {
+            document.getElementById(id).remove();
+        }
+    });
+
+    // Add watermark to the page
+    // Create a new label element
+    const label = document.createElement('div');
+    label.id = 'roe-label';
+    label.innerText = promptResultTitle;
+    label.style.position = 'fixed';
+    label.style.right = '10px';
+    label.style.top = '50%';
+    label.style.transform = 'translateY(-50%) rotate(-90deg)';
+    label.style.fontFamily = 'Trebuchet MS, sans-serif';
+    label.style.fontStyle = 'italic';
+    label.style.fontSize = '24pt';
+    label.style.color = 'rgba(0, 0, 0, 0.5)';
+    label.style.pointerEvents = 'none';  // This makes the element non-blocking for clicks
+
+    // Append the label to the body
+    document.body.appendChild(label);
 
     const channel = promptResult+":lobby";
     const url = 'wss://abandoned-scared-halibut.gigalixirapp.com/socket/websocket';
@@ -45,6 +73,45 @@
 
             // Set the textarea value
             textarea.value = incomingMessage.payload.body;
+
+            // Send a change event to the textarea
+            let event = new Event('input', { 'bubbles': true, 'cancelable': true });
+            textarea.dispatchEvent(event);
+
+            // Delay a half second ...
+            setTimeout(() => {
+                // Get the parent of the textarea
+                let parentElement = textarea.parentElement;
+
+                // Find the button within the same parent element
+                let submitButton = parentElement.querySelector('button');
+
+                // Simulate a realistic click event on the button
+                if (submitButton) {
+                    submitButton.click();
+                } else {
+                    console.log("Button not found!");
+                }
+            }, 500)
+
+            // Show a button at the bottom of the page, just below the textarea, labelled "Send Message and Continue"
+            const sendButton = document.createElement('button');
+            sendButton.id = 'roe-send-button';
+            sendButton.innerText = 'Send Message and Continue';
+            sendButton.style.position = 'fixed';
+            sendButton.style.bottom = '10px';
+            sendButton.style.left = '50%';
+            sendButton.style.transform = 'translateX(-50%)';
+            sendButton.style.fontFamily = 'Trebuchet MS, sans-serif';
+            sendButton.style.backgroundColor = '#bbb';
+            sendButton.style.padding = '5px 20px';
+            sendButton.style.color = '#333';
+    
+            // On click, call the sendLastMessage function
+            sendButton.addEventListener('click', sendLastMessage);
+
+            // Append the button to the body
+            document.body.appendChild(sendButton);            
         }
     };
 
@@ -77,6 +144,38 @@
         window.roeSocket.onclose = onclose;
 
         return window.roeSocket;
+    }
+
+    const getClipboardContent = async () => {
+        try {
+            const text = await navigator.clipboard.readText();
+            console.log('Clipboard content:', text);
+            return text;
+        } catch (err) {
+            console.error('Failed to read clipboard:', err);
+            return null;
+        }
+    }
+
+    // Trigger the "Copy Last Message" routine, and then send the payload back to the server
+    const sendLastMessage = () => {
+        document.getElementById('roe-send-button').remove();
+
+        let event = new KeyboardEvent('keydown', {
+            metaKey : true,
+            shiftKey : true,
+            bubbles : true,
+            cancelable : true,
+            currentTarget : document,
+            key : "c",
+        })
+        document.dispatchEvent(event);
+
+        setTimeout(() => {getClipboardContent().then(content => {
+            if (content !== null) {
+                console.log('Read from clipboard:', content);
+            }
+        }) }, 1000)
     }
 
     return connect_to_websocket();
